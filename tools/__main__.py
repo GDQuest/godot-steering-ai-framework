052a29f04d4bcb5b2cdae18a8f7b5d8a34d7a9ed
Nathan Lovato <nathan@gdquest.com>
2020-01-27 10:35:24 -0600

    Add class name to the generated reference, pretty print the result

--- tools/__main__.py
+++ tools/__main__.py
@@ -1,19 +1,25 @@
+import os
 from argparse import Namespace
+from pprint import pprint
 from typing import List
 
-from .modules import collect_reference
-from .modules import command_line
+from .modules import collect_reference, command_line
 
 
 def main():
     args: Namespace = command_line.parse()
+    reference = []
     for file_path in args.files:
         if not file_path.lower().endswith(".gd"):
             continue
         with open(file_path, "r") as gdscript_file:
             gdscript: List[str] = gdscript_file.readlines()
-            reference = collect_reference.get_file_reference(gdscript)
-            print(reference)
+            class_name: str = os.path.splitext(os.path.basename(file_path))[0]
+            file_data: dict = {
+                class_name: collect_reference.get_file_reference(gdscript)
+            }
+            reference.append(file_data)
+    pprint(reference)
 
 
 if __name__ == "__main__":
