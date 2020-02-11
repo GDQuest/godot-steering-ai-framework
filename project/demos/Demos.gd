9870e03e811127dfc711919d78d1a144fb8c0db8
Nathan Lovato <nathan@gdquest.com>
2020-02-11 12:51:21 -0600

    Add F11 to toggle fullscreen
    Part of #21

--- project/demos/Demos.gd
+++ project/demos/Demos.gd
@@ -11,6 +11,12 @@ func _ready() -> void:
 	button_go_back.connect("pressed", self, "_on_ButtonGoBack_pressed")
 
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("toggle_fullscreen"):
+		OS.window_fullscreen = not OS.window_fullscreen
+		get_tree().set_input_as_handled()
+
+
 func _on_DemoPickerUI_demo_requested() -> void:
 	demo_player.load_demo(demo_picker.demo_path)
 	demo_picker.hide()
